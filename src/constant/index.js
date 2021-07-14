298c3dcd24d95e2ea46c2f404494f08f52ea613f
michellewt68 <michellewt68@gmail.com>
2021-07-15 00:55:49 +0700

    add splash screen & signIn screen

---
+++ src/constant/index.js
@@ -0,0 +1,10 @@
+//color
+export const PRIMARY_COLOR = '#A067FC';
+export const WHITE = '#fff';
+export const SECONDARY_COLOR = '#E4A4FC';
+export const GREEN_COLOR = '#2DB521';
+
+//font
+export const PRIMARY_FONT = 'Inter-Regular';
+export const BOLD_FONT = 'Inter-Bold';
+export const TITLE_FONT = 'MarkoOne-Regular';
--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,13 +1,108 @@
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import * as Animatable from 'react-native-animatable';
+import {
+  PRIMARY_COLOR,
+  WHITE,
+  SECONDARY_COLOR,
+  PRIMARY_FONT,
+  BOLD_FONT,
+  TITLE_FONT,
+} from '../constant/index';
 
-const Splash = () => {
-    return (
-        <View>
-            <Text></Text>
+const SplashScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Animatable.Image
+          animation="bounceIn"
+          duraton="1500"
+          source={require('../assets/images/LogoMontra.png')}
+          style={styles.logo}
+          resizeMode="stretch"
+        />
+      </View>
+
+      <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+        <Text style={styles.title}>Keep your Financial Balance!</Text>
+        <Text style={styles.text}>Sign in With Account</Text>
+
+        <View style={styles.button}>
+          <TouchableOpacity onPress={() => navigation.navigate('SignInScreen')}>
+            <LinearGradient
+              style={styles.signIn}
+              colors={[PRIMARY_COLOR, SECONDARY_COLOR]}>
+              <Text style={styles.textStarted}>Get Started</Text>
+              <MaterialIcons name="navigate-next" color={WHITE} size={20} />
+            </LinearGradient>
+          </TouchableOpacity>
         </View>
-    )
-}
+      </Animatable.View>
+    </View>
+  );
+};
 
-export default Splash
+export default SplashScreen;
 
-const styles = StyleSheet.create({})
+const {height} = Dimensions.get('screen');
+const height_logo = height * 0.28;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: PRIMARY_COLOR,
+  },
+  header: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: WHITE,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingVertical: 50,
+    paddingHorizontal: 30,
+  },
+  logo: {
+    width: height_logo,
+    height: height_logo,
+    borderRadius: 100,
+  },
+  title: {
+    color: PRIMARY_COLOR,
+    fontSize: 30,
+    fontFamily: TITLE_FONT,
+  },
+  text: {
+    color: 'grey',
+    marginTop: 5,
+    fontFamily: PRIMARY_FONT,
+  },
+  button: {
+    alignItems: 'flex-end',
+    marginTop: 30,
+  },
+  signIn: {
+    width: 140,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    flexDirection: 'row',
+  },
+  textStarted: {
+    color: 'white',
+    fontFamily: BOLD_FONT,
+  },
+});
