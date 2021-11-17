b9c26306bea07f5764394e9da5c57390bc95ee61
Daniel22-binus <daniel025@binus.ac.id>
2021-11-17 12:25:12 +0700

    fix bug printPrice --1255000

--- src/logic/printPrice.js
+++ src/logic/printPrice.js
@@ -1,27 +1,27 @@
 const printPrice = price => {
-    let strResult = '';
-    let titik = 0;
-  
-    while (price > 9) {
-      if (titik == 3) {
-        titik = 0;
-        strResult = '.' + strResult;
-      }
-  
-      strResult = (price % 10) + strResult;
-      price = price / 10;
-      titik += 1;
-    }
-  
+  let strResult = '';
+  let titik = 0;
+
+  while (price > 9) {
     if (titik == 3) {
+      titik = 0;
       strResult = '.' + strResult;
     }
-  
-    let koma = price % 1;
-  
-    strResult = 'Rp ' + (price - koma) + strResult;
-  
-    return strResult;
-  };
-  
-  export {printPrice};
+
+    strResult = (price % 10) + strResult;
+    price = parseInt(price / 10);
+    titik += 1;
+  }
+
+  if (titik == 3) {
+    strResult = '.' + strResult;
+  }
+
+  let koma = price % 1;
+
+  strResult = 'Rp ' + (price - koma) + strResult;
+
+  return strResult;
+};
+
+export {printPrice};
